4db42d200c9cf34a9e7df23e4a7811794b095155
Eric Thuaud <eric.thuaud@insee.fr>
2021-09-27 17:09:22 +0200

    move method to new restRedirectUnit controller

--- src/main/java/fr/insee/coleman/api/controller/RestCampaign.java
+++ src/main/java/fr/insee/coleman/api/controller/RestCampaign.java
@@ -1,28 +1,18 @@
 package fr.insee.coleman.api.controller;
 
-import java.util.List;
-
-import fr.insee.coleman.api.domain.RedirectUnit;
-import fr.insee.coleman.api.services.SurveyUnitService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import fr.insee.coleman.api.configuration.JSONCollectionWrapper;
 import fr.insee.coleman.api.domain.Campaign;
 import fr.insee.coleman.api.exception.RessourceNotFoundException;
 import fr.insee.coleman.api.exception.RessourceNotValidatedException;
 import fr.insee.coleman.api.services.CampaignService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -33,8 +23,6 @@ public class RestCampaign {
 	@Autowired
 	CampaignService campaignService;
 
-	@Autowired
-	SurveyUnitService surveyUnitService;
 
 	/* GET /campaigns : To retrieve the list of ongoing campaigns */
 	@GetMapping(value = "/campaigns", produces = "application/json")
@@ -73,15 +61,7 @@ public class RestCampaign {
 		return campaignService.findContactByIdec(idec);
 	}
 
-	@GetMapping(value = "/campaigns/redirect-unit/contact/{idec}", produces = "application/json")
-	public RedirectUnit getOpenCampaignsContactsAssociatedByIdec(@PathVariable String idec) throws RessourceNotFoundException {
-		RedirectUnit ru =new RedirectUnit();
-		LOGGER.info("Request GET with idec : {}", idec);
-		ru.setOpenedCampaignsIds(campaignService.findOpenedCampaignsByIdec(idec));
-		ru.setIdContact(idec);
-		ru.setIdUe(surveyUnitService.findByIdContact(idec).getIdSu());
-		return ru;
-	}
+
 
 	/*
 	 * DELETE /campaigns : Deletes the campaign with the given idCampaign. Checks if
