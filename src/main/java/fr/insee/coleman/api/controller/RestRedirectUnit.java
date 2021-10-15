36657dd1db855775350dd144b2e30420891af428
Nicolas Turban <37210319+nicolasTurban@users.noreply.github.com>
2021-10-15 17:16:27 +0200

    remove respondent id param from redirection endpoint

--- src/main/java/fr/insee/coleman/api/controller/RestRedirectUnit.java
+++ src/main/java/fr/insee/coleman/api/controller/RestRedirectUnit.java
@@ -1,5 +1,6 @@
 package fr.insee.coleman.api.controller;
 
+import fr.insee.coleman.api.domain.Campaign;
 import fr.insee.coleman.api.domain.RedirectUnit;
 import fr.insee.coleman.api.exception.RessourceNotFoundException;
 import fr.insee.coleman.api.services.CampaignService;
@@ -7,10 +8,14 @@ import fr.insee.coleman.api.services.SurveyUnitService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -24,15 +29,17 @@ public class RestRedirectUnit {
     @Autowired
     SurveyUnitService surveyUnitService;
 
-    @GetMapping(value = "/campaigns/redirect-unit/contact/{idec}", produces = "application/json")
-    public RedirectUnit getOpenCampaignsContactsAssociatedByIdec(@PathVariable String idec) throws RessourceNotFoundException {
-        RedirectUnit ru =new RedirectUnit();
-        LOGGER.info("Request GET with idec : {}", idec);
-        ru.setOpenedCampaignsIds(campaignService.findOpenedCampaignsByIdec(idec));
-        ru.setIdContact(idec);
-        if(ru.getOpenedCampaignsIds().size()>0) {
-            ru.setIdUe(surveyUnitService.findByIdContactAndIdCampaign(idec, ru.getOpenedCampaignsIds().get(0)).getIdSu());
-        }
-        return ru;
+    @GetMapping(value = "/campaigns/redirect-unit/contact", produces = "application/json")
+    public RedirectUnit getOpenCampaignsContactsAssociatedByIdec(HttpServletRequest httpRequest) throws RessourceNotFoundException {
+
+            String idec = httpRequest.getRemoteUser();
+            RedirectUnit ru =new RedirectUnit();
+            LOGGER.info("Request redirect-unit with idec : {}", idec);
+            ru.setOpenedCampaignsIds(campaignService.findOpenedCampaignsByIdec(idec));
+            ru.setIdContact(idec);
+            if(ru.getOpenedCampaignsIds().size()>0) {
+                ru.setIdUe(surveyUnitService.findByIdContactAndIdCampaign(idec, ru.getOpenedCampaignsIds().get(0)).getIdSu());
+            }
+            return ru;
     }
 }
