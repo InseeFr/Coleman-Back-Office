870e42f1d051816b02265aa3c6cffac75c4f4c63
Eric Thuaud <eric.thuaud@insee.fr>
2022-02-18 11:16:15 +0100

    add healthcheck controller

--- src/main/java/fr/insee/coleman/api/controller/HealthCheckController.java
+++ src/main/java/fr/insee/coleman/api/controller/HealthCheckController.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Laurent Caouissin
  *
  */
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
-@RequestMapping(path = "/api")
 public class HealthCheckController {
     private static final Logger LOGGER = LoggerFactory.getLogger(HealthCheckController.class);
 
