a447cbbdcd04bb6a36f1eba0f84eb262ff09798c
Eric Thuaud <eric.thuaud@insee.fr>
2022-04-07 12:40:02 +0200

    implement rest campaign ongoing endpoint

---
+++ src/main/java/fr/insee/coleman/api/dto/campaign/OngoingDTO.java
@@ -0,0 +1,13 @@
+package fr.insee.coleman.api.dto.campaign;
+
+public class OngoingDTO {
+    private boolean ongoing;
+
+    public boolean isOngoing() {
+        return ongoing;
+    }
+
+    public void setOngoing(boolean ongoing) {
+        this.ongoing = ongoing;
+    }
+}
